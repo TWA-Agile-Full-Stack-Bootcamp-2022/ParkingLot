b7a755884cb992ad7a87b60fbbff7cfae5027532
Guangguang Ma <ggma@thoughtworks.com>
2022-10-14 14:42:07 +0800

    test(story 3): add test: parking boy park car to second parking lot when first parking lot without positions

--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -21,5 +21,21 @@ namespace ParkingLotTest
             Assert.Equal(0, firstParkingLot.Capacity);
             Assert.Equal(2, secondParkingLot.Capacity);
         }
+
+        [Fact]
+        public void Should_park_to_the_second_parking_lot_given_parking_boy_manage_2_parking_lot_and_first_is_full()
+        {
+            var firstParkingLot = new ParkingLot.ParkingLot(0);
+            var secondParkingLot = new ParkingLot.ParkingLot(2);
+            var parkingBoy = new ParkingBoy();
+            parkingBoy.AddManagedParkingLot(firstParkingLot);
+            parkingBoy.AddManagedParkingLot(secondParkingLot);
+
+            var ticket = parkingBoy.Park(new Car("江AB1234"));
+
+            Assert.NotEmpty(ticket.TicketNo);
+            Assert.Equal(0, firstParkingLot.Capacity);
+            Assert.Equal(1, secondParkingLot.Capacity);
+        }
     }
 }
