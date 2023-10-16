4fb98e3f78f2d38aa6217c080821641fbaf2a984
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-16 17:22:58 +0800

    feat: should can fetch the car by given ticket

--- ParkingLot/ParkingLot.cs
+++ ParkingLot/ParkingLot.cs
@@ -1,10 +1,31 @@
-﻿namespace ParkingLot
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
 {
     public class ParkingLot
     {
+        private Dictionary<Ticket, Car> ticketCarPairs = new Dictionary<Ticket, Car>();
+
+        public ParkingLot()
+        {
+        }
+
+        public ParkingLot(Dictionary<Ticket, Car> ticketCarPairs)
+        {
+            this.ticketCarPairs = ticketCarPairs;
+        }
+
+        public Car Fetch(Ticket ticketRecived)
+        {
+            return ticketCarPairs[ticketRecived];
+        }
+
         public Ticket Park(Car car)
         {
-            return new Ticket();
+            Ticket ticket = new Ticket();
+            ticketCarPairs.Add(ticket, car);
+            return ticket;
         }
     }
 }
--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -1,6 +1,7 @@
 namespace ParkingLotTest
 {
     using ParkingLot;
+    using System.Collections.Generic;
     using Xunit;
 
     public class ParkingLotTest
@@ -15,6 +16,22 @@ namespace ParkingLotTest
             Ticket ticketRecived = parkingLot.Park(car);
             // Then
             Assert.NotNull(ticketRecived);
+        }
+
+        [Fact]
+        public void Should_return_the_right_car_when_fetch_by_given_ticket()
+        {
+            // Given
+            Dictionary<Ticket, Car> ticketCarPairs = new Dictionary<Ticket, Car>();
+            Ticket givenTicket = new Ticket();
+            Car givenCar = new Car();
+            ticketCarPairs.Add(givenTicket, givenCar);
+            ParkingLot parkingLot = new ParkingLot(ticketCarPairs);
+
+            // When
+            Car fetchedCar = parkingLot.Fetch(givenTicket);
+            // Then
+            Assert.Equal(givenCar, fetchedCar);
         }
     }
 }
