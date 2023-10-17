fea7a9cbf2344d4217890a0472673eed09d98dc9
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-17 15:25:25 +0800

    test: should manager can ask parking boy to park

---
+++ ParkingLot/ParkingLotManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    public class ParkingLotManager
+    {
+        private readonly List<ParkingBoy> parkingBoys;
+
+        public ParkingLotManager(List<ParkingBoy> parkingBoys)
+        {
+            this.parkingBoys = parkingBoys;
+        }
+
+        public Ticket Park(Car car)
+        {
+            ParkingBoy parkingBoy = FindAvailableParkingBoy();
+            return parkingBoy.Park(car);
+        }
+
+        private ParkingBoy FindAvailableParkingBoy()
+        {
+            return null;
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotManagerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    using ParkingLot;
+
+    public class ParkingLotManagerTest
+    {
+        [Fact]
+        public void Should_can_park_by_some_type_of_parking_boy_when_manager_park_given_a_some_of_parking_boy()
+        {
+            // Needs to clarify requirements: How to specify a parking boy ?
+            // Given
+            List<ParkingLot> parkingLotsOfParkingBoy = new List<ParkingLot> { new ParkingLot(), new ParkingLot() };
+            List<ParkingLot> parkingLotsOfSmartParkingBoy = new List<ParkingLot> { new ParkingLot(), new ParkingLot() };
+            List<ParkingLot> parkingLotsOfSuperParkingBoy = new List<ParkingLot> { new ParkingLot(), new ParkingLot() };
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLotsOfParkingBoy);
+            ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotsOfSmartParkingBoy);
+            ParkingBoy superParkingBoy = new SuperSmartParkingBoy(parkingLotsOfSuperParkingBoy);
+
+            ParkingLotManager parkingLotManager = new ParkingLotManager(new List<ParkingBoy> { parkingBoy, smartParkingBoy, superParkingBoy });
+            Car car = new Car();
+            // When
+            Ticket ticket = parkingLotManager.Park(car);
+            // Then
+            Assert.NotNull(ticket);
+        }
+    }
+}
