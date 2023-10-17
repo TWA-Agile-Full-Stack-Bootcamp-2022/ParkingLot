1543cc1c2df5118eb5a7c8c1d013ef4366784849
Hong Wu <hong.wu@rea-group.com>
2023-10-17 20:51:38 +0800

    feat: make the parking boy be able to handle multiple parking lots

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class ParkingBoy
     {
-        public ParkingLot ParkingLot { get; set; }
+        public List<ParkingLot> ParkingLots { get; set; }
 
-        public ParkingTicket ParkCar(Car car) => ParkingLot.ParkCar(car);
+        public ParkingTicket ParkCar(Car car) => ParkingLots.First().ParkCar(car);
 
-        public Car FetchCar(ParkingTicket ticket) => ParkingLot.FetchCar(ticket);
+        public Car FetchCar(ParkingTicket ticket) => ParkingLots.First().FetchCar(ticket);
     }
 }
