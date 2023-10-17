bf305592b38d3a93e6673ed1c8affc3970a21a4c
Hong Wu <hong.wu@rea-group.com>
2023-10-17 22:19:21 +0800

    test: park cars to the parking lot which contains more empty positions

---
+++ ParkingLotTest/SmartParkingStrategyTest.cs
@@ -0,0 +1,28 @@
+namespace ParkingLotTest
+{
+    using System.Collections.Generic;
+    using Moq;
+    using ParkingLot;
+    using Xunit;
+
+    public class SmartParkingStrategyTest
+    {
+        [Fact]
+        public void Park_cars_to_parking_lot_which_contains_more_empty_positions()
+        {
+            // given
+            // NOTE: mock class
+            var parkingLot1 = new Mock<ParkingLot>();
+            parkingLot1.Setup(p => p.NumOfEmptyPositions).Returns(5);
+            var parkingLot2 = new Mock<ParkingLot>();
+            parkingLot2.Setup(p => p.NumOfEmptyPositions).Returns(10);
+            IParkingStrategy strategy = new SmartParkingStrategy();
+
+            // when
+            var parkingLot = strategy.Which(new List<ParkingLot> { parkingLot1.Object, parkingLot2.Object });
+
+            // then
+            Assert.Equal(parkingLot2.Object, parkingLot);
+        }
+    }
+}
