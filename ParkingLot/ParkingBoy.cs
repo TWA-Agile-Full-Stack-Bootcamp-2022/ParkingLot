f53ef446aa3bc4f6e4c85ef1f266e7ab28edfd4b
yoogodong <yoogdong@gmail.com>
2023-10-20 12:24:46 +0800

    feat: AC1

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -1,7 +1,21 @@
 ﻿namespace ParkingLot
 {
     using System;
+    using System.Collections.Generic;
+
     public class ParkingBoy
     {
+        private readonly Dictionary<string, string> parkingLot = new Dictionary<string, string>();
+        public string Park(string car)
+        {
+            string ticket = car + "ticket";
+            parkingLot.Add(ticket, car);
+            return ticket;
+        }
+
+        public string Fetch(string ticket)
+        {
+            return parkingLot[ticket];
+        }
     }
 }
--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -6,10 +6,13 @@ namespace ParkingLotTest
     public class ParkingBoyTest
     {
         [Fact]
-        public void Test1()
+        public void Should_fetch_the_same_car_after_parked()
         {
-            var class1 = new ParkingBoy();
-            Assert.NotNull(class1);
+            var boy = new ParkingBoy();
+            const string Car1 = "car1";
+            string ticket1 = boy.Park(Car1);
+            string car1 = boy.Fetch(ticket1);
+            Assert.Equal(Car1, car1);
         }
     }
 }
