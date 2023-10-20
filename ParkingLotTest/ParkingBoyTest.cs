7dbc234add701ebe6aff83e7a2357bb73b21eac0
yoogodong <yoogdong@gmail.com>
2023-10-20 12:29:23 +0800

    feat: AC2

--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -1,6 +1,7 @@
 namespace ParkingLotTest
 {
     using ParkingLot;
+    using System.Reflection;
     using Xunit;
 
     public class ParkingBoyTest
@@ -13,6 +14,22 @@ namespace ParkingLotTest
             string ticket1 = boy.Park(Car1);
             string car1 = boy.Fetch(ticket1);
             Assert.Equal(Car1, car1);
+        }
+
+        [Fact]
+        public void Should_fetch_the_matched_car_with_corresponding_ticket()
+        {
+            ParkingBoy boy = new ParkingBoy();
+            const string Car1 = "Car1";
+            string ticket1 = boy.Park(Car1);
+            const string Car2 = "Car2";
+            string ticket2 = boy.Park(Car2);
+
+            string car1 = boy.Fetch(ticket1);
+            string car2 = boy.Fetch(ticket2);
+
+            Assert.Equal(Car1, car1);
+            Assert.Equal(Car2, car2);
         }
     }
 }
